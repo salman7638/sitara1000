4021d1b5cefc088d79c525f356594be8db3a9bfa
salman7638 <salman.m@dynexcel.co>
2022-04-01 15:49:20 +0000

    updated

---
+++ de_property_report/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "de_property_report",
+
+    'summary': """
+            de_property_report
+             1) booking_report
+             2) allotment_report
+       """,
+
+    'description': """
+       de_property_report
+             1) booking_report
+             2) allotment_report
+    """,
+
+    'author': "Dynexcel",
+    'website': "http://www.dynexcel.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Sale',
+    'version': '15.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','sale','de_sale_booking'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'report/booking_report.xml',
+        'report/booking_template.xml',
+        'report/allotment_report.xml',
+        'report/allotment_template.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
