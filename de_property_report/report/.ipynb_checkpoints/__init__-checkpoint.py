c4745a2d7c27e5fa78d3a5a3b04d53ae20103042
hamayunfazal <hamayunfazal@gmail.com>
2022-04-14 15:42:11 +0000

    update bundle

--- de_property_report/report/.ipynb_checkpoints/__init__-checkpoint.py
+++ de_property_report/report/.ipynb_checkpoints/__init__-checkpoint.py
@@ -5,3 +5,4 @@ from . import advance_receivable_report
 from . import plot_detail_report
 from . import plot_pdf_status_report
 from . import partner_ledger_report
+from . import property_sale_report
