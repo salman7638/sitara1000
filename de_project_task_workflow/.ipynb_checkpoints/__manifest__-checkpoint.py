746958d6770a042acd1f434df3371a2598ce0ba0
salman7638 <79008117+salman7638@users.noreply.github.com>
2022-04-16 09:46:03 +0500

    project planning

--- de_project_task_workflow/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ de_project_task_workflow/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -12,10 +12,6 @@
 
     'author': "Dynexcel",
     'website': "http://www.dynexcel.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Project',
     'version': '14.0.0.3',
 
@@ -28,8 +24,7 @@
         'views/project_views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
 }
